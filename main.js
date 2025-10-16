b245e1cebfe5e5c9ee67a0f0b283271901787b63
Cuong Tran <cuongtran0961@gmail.com>
2025-10-16 22:30:11 +0700

    Code mới

---
+++ main.js
@@ -0,0 +1,13 @@
+function thongBao() {
+    alert("Chào mừng bạn đến với trang web của tôi!");
+    console.log("Chào mừng bạn đến với trang web của tôi!");
+}
+
+thongBao();
+
+
+function tinhTong(a, b) {
+    return a + b;
+}
+
+console.log("Tổng của 5 và 10 là: " + tinhTong(5, 10));
